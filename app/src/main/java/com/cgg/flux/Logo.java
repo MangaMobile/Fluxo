520200bec9e0c0808c5775559c9fde34dedcc1e1
giovanee <giovanemandrade@gmail.com>
2015-11-02 22:16:56 -0200

    Fluxo

---
+++ app/src/main/java/com/cgg/flux/Logo.java
@@ -0,0 +1,13 @@
+package com.cgg.flux;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
